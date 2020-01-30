11cd729bcce90ccbeadd2afc3c9e18527b01a5d0
Ben Ramsay <benramsay378@gmail.com>
2020-01-30 15:13:52 -0800

    Added week 4 exercises.

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -2,6 +2,9 @@
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
+# install.packages("nycflights13")
+library('nycflights13')
+library('dplyr')
 
 
 # The data frame `flights` should now be accessible to you.
@@ -9,16 +12,17 @@
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+View(flights)
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(flights, delay_in_air = arr_delay - dep_delay)
+View(flights)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- arrange(flights, -delay_in_air)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
@@ -29,14 +33,20 @@
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+summarize(flights, avg = mean(delay_in_air, na.rm = TRUE))
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
+to_SEA <- filter(flights, dest == 'SEA')
+to_SEA <- select(to_SEA, origin, dest, delay_in_air)
 
+flights %>%
+  filter(dest == "SEA") %>%
+  summarize(avg_delayed = mean(delay_in_air, na.rm = TRUE)) %>%
+  pull(avg_delayed)
 
 # On average, did flights to SeaTac gain or loose time?
-
+summarize(to_SEA, avg_SEA = mean(delay_in_air, na.rm = TRUE))
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
