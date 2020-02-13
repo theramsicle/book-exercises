b592fbc5a01e92d2d97f6b6d3b65e1585b353f8c
Ben Ramsay <benramsay378@gmail.com>
2020-02-13 15:13:03 -0800

    Finished week 6 exercises.

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -8,26 +8,40 @@ library("jsonlite")
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
 
+source('apikey.R') # go to that file, select all, and hit ctrl-enter
+
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- 'Spotlight'
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
 # The resource is `reviews/search.json`
 # See the interactive console for parameter details:
 #   https://developer.nytimes.com/movie_reviews_v2.json
-#
+
+base_uri <- 'https://api.nytimes.com/svc/movies/v2/'
+endpoint <- 'reviews/search.json'
+uri <- paste0(base_uri, endpoint)
+
+query_params_list <- list('api-key' = nyt_key, 'movie_name' = movie_name)
+
+
+
+
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
-
+install.packages('ggplot2')
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
 
+response <- GET(uri, query = query_params_list)
+body <- content(response, 'text')
+data <- fromJSON(body)
 
 # What kind of data structure did this produce? A data frame? A list?
-
+data
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
